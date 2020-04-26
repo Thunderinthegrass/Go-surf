35467593dc6a155a831ab4e767cbbcc2e4b95ed5
ThunderInTheGrass <propeller8924@gmail.com>
2020-04-26 12:46:46 +0800

    header, частично surf

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left"src="img/arrows-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right"src="img/arrows-right.svg" alt="">',
+    asNavFor:'.slider-dots'
+  });
+  $('.slider-dots').slick({
+    asNavFor:'.header__slider',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  })
+});
