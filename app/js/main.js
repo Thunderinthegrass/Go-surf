82b818c989523629a411e8bb0907b87d7dd54b12
ThunderInTheGrass <propeller8924@gmail.com>
2020-05-05 21:43:34 +0800

    добавил и заанимировал иконки главного меню, только иконку "о болотах " не заанимировал

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,24 @@ $(function(){
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left"src="img/arrows-left.svg" alt="">',
     nextArrow: '<img class="slider-arrows slider-arrows__right"src="img/arrows-right.svg" alt="">',
-    asNavFor:'.slider-dots'
+    asNavFor:'.slider-dotshead'
   });
-  $('.slider-dots').slick({
+  $('.slider-dotshead').slick({
     asNavFor:'.header__slider',
     slidesToShow: 4,
     slidesToScroll: 1,
+  })
+  $('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left"src="img/arrows-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right"src="img/arrows-right.svg" alt="">',
+    asNavFor:'.slider-map'
+  });
+  $('.slider-map').slick({
+    asNavFor:'.surf-slider',
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    focusOnSelect: true
   })
 });
