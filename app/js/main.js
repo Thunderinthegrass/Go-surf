51cf033c972cae08d626b1547a579428ff158fc4
ThunderInTheGrass <propeller8924@gmail.com>
2020-06-15 18:05:21 +0800

    сделал секцию "travel"

--- app/js/main.js
+++ app/js/main.js
@@ -24,4 +24,10 @@ $(function(){
     slidesToScroll: 1,
     focusOnSelect: true
   })
+  $('.travel__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left"src="img/arrows-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right"src="img/arrows-right.svg" alt="">'
+  });
 });
